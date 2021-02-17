3106011217123109f427f126bedba09a51b5a5df
Alan <alanrd772@gmail.com>
2021-02-17 16:42:50 +0400

    optimize code, add missing file handling

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
-const messages = JSON.parse(fs.readFileSync('result.json', 'utf8')).messages;
+messages = null;
+try {
+    messages = JSON.parse(fs.readFileSync('result.json', 'utf8')).messages;
+} catch (err) {
+    return console.log("Messages JSON file not found. Is it named result.json, in the root folder of the program and exported as JSON via Telegram Desktop?")
+}
 
 let result = {
     messages: { },
@@ -13,40 +18,48 @@ for (i in messages) {
     let message = messages[i]
     let from = message.from
     if (!from && message.action.includes("call")) {
-        result.calls[message.actor] = result.calls[message.actor] !== undefined ? result.calls[message.actor] + 1 : 1
+        addVal("calls", from, 1)
         // phone calls use "actor" for some reason
         continue // skip, go to next loop
     }
-    result.messages[from] = result.messages[from] !== undefined ? result.messages[from] + 1 : 1
+    addVal("messages", from, 1)
 
     if (message.media_type || (message.mime_type && message.mime_type.includes("image")) || message.photo) {
-        result.media[from] = result.media[from] !== undefined ? result.media[from] + 1 : 1
+        addVal("media", from, 1)
         if (message.media_type) {
             let type = message.media_type
             if (type == "animation") type = "video_file"
             if (!result.media[type]) result.media[type] = {} // if key doesn't exist make it
-            result.media[type][from] = result.media[type][from] !== undefined ? result.media[type][from] + 1 : 1
+            addVal(`meda.${type}`, from, 1)
         } else if ((message.mime_type && message.mime_type.includes("image")) || message.photo) {
             if (!result.media.image) result.media.image = {} // if key doesn't exist make it
-            result.media.image[from] = result.media.image[from] !== undefined ? result.media.image[from] + 1 : 1
+            addVal("media.image", from, 1)
         }
     }
 
     if (message.via_bot) {
         if (!result.bot[message.via_bot]) result.bot[message.via_bot] = {}
-        result.bot[message.via_bot][from] = result.bot[message.via_bot][from] !== undefined ? result.bot[message.via_bot][from] + 1 : 1
+        addVal(`bot.${message.via_bot}`, from, 1)
     }
 
     let text = message.text
     for (i in text) {
         if (text[i].type && text[i].type == "link") {
-            result.links[from] = result.links[from] !== undefined ? result.links[from] + 1 : 1
+            addVal("links", from, 1)
             if (text[i].text.includes("youtu.be") || text[i].text.includes("youtube.com")) {
                 if (!result.links.youtube) result.links.youtube = {} // if key doesn't exist make it
-                result.links.youtube[from] = result.links.youtube[from] !== undefined ? result.links.youtube[from] + 1 : 1
+                addVal("links.youtube", from, 1)
             }
         }
     }
+}
+
+function addVal(key, from, value) {
+    key = key.split(".")
+    if (key.length > 1)
+        result[key[0]][key[1]][from] = result[key[0]][key[1]][from] !== undefined ? result[key[0]][key[1]][from] + value : value
+    else
+        result[key[0]][from] = result[key[0]][from] !== undefined ? result[key[0]][from] + value : value
 }
 
 console.log(result)
